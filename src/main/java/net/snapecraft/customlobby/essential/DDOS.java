0619dea5938425a483dc67c73ff434d9b72f2be1
Tallerik <erik.klasse@gmail.com>
2019-04-06 23:55:08 +0200

    Hotfix DDOS

--- src/main/java/net/snapecraft/customlobby/essential/DDOS.java
+++ src/main/java/net/snapecraft/customlobby/essential/DDOS.java
@@ -1,9 +1,11 @@
 package net.snapecraft.customlobby.essential;
 
 import net.snapecraft.customlobby.utils.API;
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 public class DDOS implements CommandExecutor {
     @Override
@@ -11,9 +13,15 @@ public class DDOS implements CommandExecutor {
 
         if(sender.hasPermission("CustomLobby.DDOS")) {
             if(args.length == 1) {
-                sender.sendMessage(API.getPrefix() + "§aDer Spieler wird jetzt geddost!");
+                Player target = Bukkit.getPlayer(args[0]);
+                if(target != null) {
+                    target.kickPlayer("Timed out!");
+                    sender.sendMessage(API.getPrefix() + " §aDer Spieler wird jetzt geddost!");
+                } else {
+                    sender.sendMessage(API.getPrefix() + " §cSpieler nicht gefunden!");
+                }
             } else {
-                sender.sendMessage("§cWrong Usage (Du kannst dich doch net selber DDOSSEN!): /ddos <Spieler>");
+                sender.sendMessage(API.getPrefix() + " §cWrong Usage (Du kannst dich doch net selber DDOSSEN!): /ddos <Spieler>");
             }
         } else {
             sender.sendMessage(API.getNoPermString());
