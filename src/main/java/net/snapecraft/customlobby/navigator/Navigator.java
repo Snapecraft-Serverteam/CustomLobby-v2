3bc9d06723b17a2129079f55a8c63fa7e78ec943
Tallerik <erik.klasse@gmail.com>
2019-08-17 11:29:05 +0200

    Navigator with custom server info (UNFINISHED)

--- src/main/java/net/snapecraft/customlobby/navigator/Navigator.java
+++ src/main/java/net/snapecraft/customlobby/navigator/Navigator.java
@@ -1,16 +1,22 @@
 package net.snapecraft.customlobby.navigator;
 
+import de.dytanic.cloudnet.api.CloudAPI;
+import de.dytanic.cloudnet.bridge.CloudServer;
+import de.dytanic.cloudnet.lib.server.info.ServerInfo;
 import net.snapecraft.customlobby.CustomLobby;
 import net.snapecraft.customlobby.utils.ItemAPI;
-import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
-import org.bukkit.DyeColor;
-import org.bukkit.Material;
+import org.bukkit.*;
 import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import static de.dytanic.cloudnet.api.CloudAPI.getInstance;
 
 public class Navigator {
     public static void createNavigatorGUI(Player p)
@@ -34,7 +40,45 @@ public class Navigator {
             ConfigurationSection row = navSection.getConfigurationSection(Integer.toString(j));
             for (int k = 0; k < 9; k++) {
                 ConfigurationSection obj = row.getConfigurationSection(Integer.toString(k));
-                inv.setItem(slot, ItemAPI.createItem(Material.getMaterial(obj.getString("material")), ChatColor.translateAlternateColorCodes('&', obj.getString("name")), (byte)obj.getInt("meta"), 1));
+                if(obj.getString("action") != null && obj.getString("action").startsWith("connserver")) {
+                    String server = obj.getString("action").replaceAll("connserver ", "");
+                    List<String> lore = new ArrayList<>();
+                    System.out.println(obj.getString("action")+  "   "+ server);
+
+                    Collection<ServerInfo> servers = CloudAPI.getInstance().getCloudServers();
+                    List<ServerInfo> match = new ArrayList<>();
+                    int count = 1;
+                    for (int y = 0; y < servers.size(); i++) {
+                        for (ServerInfo s : servers) {
+                            System.out.println(s.getServiceId().getServerId() +" == "+ server.replaceAll("\\*", Integer.toString(count)));
+                            if(s.getServiceId().getServerId().equals(server.replaceAll("\\*", Integer.toString(count)))) {
+                                match.add(s);
+                                count++;
+                            }
+                        }
+                    }
+
+                    if(match.size() > 0) {
+                        for(ServerInfo info : match) {
+                            lore.add("&6" + info.getServiceId().getServerId() + "&7: &4(&a" + info.getOnlineCount() + " &7/ &a" + info.getMaxPlayers() + "&4)");
+                        }
+
+
+                        System.out.println(obj.getString("material") + (byte) obj.getInt("meta"));
+                        inv.setItem(slot,
+                                ItemAPI.createItemWithLore(
+                                        Material.getMaterial(obj.getString(obj.getString("material"))),
+                                        ChatColor.translateAlternateColorCodes('&', obj.getString("name")),
+                                        (byte) obj.getInt("meta"),
+                                        1,
+                                        lore
+                                )
+                        );
+                    }
+
+                } else {
+                    inv.setItem(slot, ItemAPI.createItem(Material.getMaterial(obj.getString("material")), ChatColor.translateAlternateColorCodes('&', obj.getString("name")), (byte) obj.getInt("meta"), 1));
+                }
                 slot++;
             }
         }
